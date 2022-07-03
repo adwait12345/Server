00430410800e32ae867737f5ebe71fe4bdb1b86d
adwait12345 <89302800+adwait12345@users.noreply.github.com>
2022-07-03 18:15:29 +0530

    commit

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.use(express.json())
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-app.listen(3001, ()=> console.log('Server Started'))
+app.listen(process.env.PORT || 3001, ()=> console.log('Server Started'))
