caa1841a9575b905e14b7c0e37b38b169dc74977
adwait12345 <89302800+adwait12345@users.noreply.github.com>
2022-07-03 21:11:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-const db = mongoose.connection
-db.on('error', (error)=> console.error(error))
-db.once('open', ()=> console.log("Connected to database!"))
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database!"));
 
+app.use(express.json());
 
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.status(200).send("Hello server is running").end();
+});
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
 
-let PORT = process.env.PORT ;
-app.listen(PORT || 5000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+let PORT = process.env.PORT;
+app.listen(PORT || 5000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
